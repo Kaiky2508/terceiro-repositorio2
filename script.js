7520a365666d799434e2f65ca52e42346532fe88
Kaiky2508 <127265625+Kaiky2508@users.noreply.github.com>
2024-08-06 22:51:43 -0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaALternativas = document.querySelector(".caixa-alternativas");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
